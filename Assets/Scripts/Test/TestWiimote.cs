2864daceb1f49ce50771e653428188f222a74cce
helisoya <oudot.julien@laposte.net>
2024-09-24 11:25:05 +0200

    Test

--- Assets/Scripts/Test/TestWiimote.cs
+++ Assets/Scripts/Test/TestWiimote.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using WiimoteApi;
 
 public class TestWiimote : MonoBehaviour
 {
+    Wiimote wiimote;
     // Start is called before the first frame update
     void Start()
     {
-
+        WiimoteManager.FindWiimotes();
+        wiimote = WiimoteManager.Wiimotes[0];
+        wiimote.SendPlayerLED(true, false, false, false);
     }
 
     // Update is called once per frame
