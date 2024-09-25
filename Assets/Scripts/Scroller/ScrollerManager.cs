ae9d1d065700ed0b515245139ec641f20ec27929
helisoya <oudot.julien@laposte.net>
2024-09-25 10:25:24 +0200

    Curve Shader

--- Assets/Scripts/Scroller/ScrollerManager.cs
+++ Assets/Scripts/Scroller/ScrollerManager.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class ScrollerManager : MonoBehaviour
 {
     [Header("Components")]
-    [SerializeField] private Transform prefabBlock;
-    private Transform[] blocks;
+    [SerializeField] private RoadBlock prefabBlock;
+    private RoadBlock[] blocks;
 
     [Header("Infos")]
     [SerializeField] private float scrollSpeed;
     [SerializeField] private float blockLength;
     [SerializeField] private int maxBlocks;
+    [SerializeField] private float curveStrength;
     private Vector3 direction = new Vector3(-1, 0, 0);
+    private int lastIdx = 0;
 
     void Start()
     {
-        blocks = new Transform[maxBlocks];
+        blocks = new RoadBlock[maxBlocks];
 
         float position = -blockLength;
         for (int i = 0; i < maxBlocks; i++)
@@ -29,13 +31,25 @@ public class ScrollerManager : MonoBehaviour
 
     void Update()
     {
-        foreach (Transform child in blocks)
+        RoadBlock child;
+        for (int i = 0; i < blocks.Length; i++)
         {
-            child.position += direction * scrollSpeed * Time.deltaTime;
-            if (child.position.x < -blockLength * 2)
+            child = blocks[i];
+            child.transform.position += direction * scrollSpeed * Time.deltaTime;
+            if (child.transform.position.x < -blockLength * 2)
             {
-                child.position += new Vector3(maxBlocks * blockLength, 0, 0);
+                child.transform.position += new Vector3(maxBlocks * blockLength, 0, 0);
             }
+
+            int number = (int)(child.transform.position.x / 200);
+
+            if (number > 0)
+            {
+                child.SetCurveValue(number / (float)maxBlocks * curveStrength);
+            }
+
         }
     }
+
 }
+
