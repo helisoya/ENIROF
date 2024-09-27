f274e28cbec136f309f7d64ed73d30e9c58ca537
helisoya <oudot.julien@laposte.net>
2024-09-27 10:48:35 +0200

    Obstacles fix

--- Assets/Scripts/Obstacles/ObstacleManager.cs
+++ Assets/Scripts/Obstacles/ObstacleManager.cs
@@ -21,9 +21,9 @@ public class ObstacleManager : MonoBehaviour
         if (score <= 100) return 1f;
         if (score <= 250) return 0.75f;
         if (score <= 500) return 0.5f;
-        if (score <= 750) return 0.25f;
+        if (score <= 750) return 0.3f;
 
-        return 1f;
+        return 0.2f;
     }
 
     void Update()
@@ -35,7 +35,7 @@ public class ObstacleManager : MonoBehaviour
         if (Time.time - cooldownStart >= cooldown)
         {
             Obstacle obj = Instantiate(obstaclePrefab,
-                new Vector3(250, 5f, spawns[Random.Range(0, spawns.Length)]),
+                new Vector3(500, 5f, spawns[Random.Range(0, spawns.Length)]),
                 Quaternion.Euler(0, 90, 0),
                 transform
             );
